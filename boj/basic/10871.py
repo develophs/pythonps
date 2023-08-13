01789722bfe84df2cd752df7254f0429c27ad3a6
HanseongBae <bha4388@naver.com>
2023-08-13 20:35:52 +0900

    백준 기본 입출력 10871번

---
+++ boj/basic/10871.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    N, X = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    for i in range(N):
+        if (A[i] < X):
+            print(A[i], end=" ")
