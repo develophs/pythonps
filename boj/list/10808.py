c8bbff936a1ac122214ac6151c0d76805fb07933
HanseongBae <bha4388@naver.com>
2023-08-16 22:51:06 +0900

    10808 알파벳

---
+++ boj/list/10808.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    S = input()
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z']
+    count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for s in S.strip():
+        index = alphabet.index(s)
+        count[index] += 1
+
+    for num in count:
+        print(num, end=" ")
