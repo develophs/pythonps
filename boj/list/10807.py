f0e4692431a96489585e02600f38070f9785362f
HanseongBae <bha4388@naver.com>
2023-08-28 21:51:51 +0900

    시간복잡도 추가

--- boj/list/10807.py
+++ boj/list/10807.py
@@ -1,8 +1,10 @@
 if __name__ == '__main__':
+    # O(N)
     N = int(input())
     nums = list(map(int, input().split()))
     v = int(input())
     sum = 0
+
     for i in range(N):
         if (nums[i] == v):
             sum += 1
