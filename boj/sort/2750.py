295439094880ff39a6bff813a33175b05ac7b2b1
HanseongBae <bha4388@naver.com>
2023-10-30 07:39:05 +0900

    [Bronze2] BOJ 2750 수 정렬하기 / 정렬 O(logN) / 메모리 : 31120 KB / 시간 : 60ms

---
+++ boj/sort/2750.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+if __name__ == '__main__':
+
+    # O(logN)
+    # 숫자를 추가하는것은 O(1)이므로 거의 무시된다.
+    # 파이썬 내장 정렬알고리즘의 시간 복잡도는 O(logN)이다.
+    n = int(stdin.readline())
+    my_nums = []
+    for _ in range(n):
+        my_nums.append(int(stdin.readline()))
+
+    my_nums.sort()
+
+    for num in my_nums:
+        print(num)
